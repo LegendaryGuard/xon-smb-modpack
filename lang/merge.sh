623d5f6c5b6d22759de13280f9eab2da797b9354
LegendGuard <49716252+LegendaryGuard@users.noreply.github.com>
2025-02-02 12:46:52 +0100

    Update localizable-strings branch from NaitLee:NaitLee/more-localizable-strings branch

--- lang/merge.sh
+++ lang/merge.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+# Merge mod translation and vanilla translation to one file for distribution
+
 cwd="`pwd`"
 cd "`dirname $0`/.."
-
 base=xonotic
-if [ ! -d "$base" ]; then
-    echo "RTFM (README.md)"
+
+fail() {
+    echo $2
     cd $cwd
-    exit 1
+    exit $1
+}
+
+if [ ! -d "$base" ]; then
+    fail 1 "Not proceeding because xonotic data is missing. See 'README.md'"
+fi
+if ! msgcat --version >>/dev/null; then
+    fail 0 "Not merging translation files because 'gettext' is not installed"
 fi
 
 pk3name=zzz-smb-l10n
@@ -30,9 +39,9 @@ if [ ! -n "${ZIP:-}" ]; then
     if [ -e "compiled/${pk3name}.*.pk3" ]; then
         rm -v compiled/${pk3name}.*.pk3
     fi
-    # unique id for different versions of localization
-    sha256=$(cat compiled/${pk3name}.pk3dir/* | sha256sum)
-    pk3path=compiled/$pk3name.${sha256:0:8}.pk3
+    hash=$(cat compiled/${pk3name}.pk3dir/* | sha256sum)
+    version=$(date +%y%m%d)+${hash:0:4}
+    pk3path=compiled/${pk3name}_${version}.pk3
     echo "Making localization package $pk3path"
     echo "localization package for smb server" >$dst/${pk3name}.pk3.serverpackage
     zip -9 -j $pk3path $dst/*
